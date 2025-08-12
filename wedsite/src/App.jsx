41a39b173b2ab0cd6850c38099b73db2cf09f232
arr_cole <andrew.rr.cole@gmail.com>
2025-08-12 12:32:53 -0400

    removed div creating alignment issues

--- wedsite/src/App.jsx
+++ wedsite/src/App.jsx
@@ -31,10 +31,7 @@ function App() {
               </img>
             </Card>
             {/* desktop viewer */}
-            <motion.div 
-            initial={{ opacity: 0, y: 50 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.6, ease: "easeOut" }}
+            <div 
             className='relative w-full md:w-1/2 p-4 text-center m-auto'>                      
               <img
               src="/desktopApp.png" 
@@ -42,7 +39,7 @@ function App() {
               className='w-full h-auto justify-center m-auto z-30'
               />
               <a 
-              className='absolute md:bottom-[60px] bottom-[60px] md:right-[60px] right-[60px] md:w-[240px] w-[180px]'
+              className='absolute md:bottom-[60px] bottom-[60px] md:right-[60px] right-[30px] md:w-[240px] w-[160px]'
               href="https://andrew-and-carolyn.wedsites.com/"
               target='_blank'                
               >
@@ -51,14 +48,14 @@ function App() {
                 />
               </a>
               <a 
-              className='absolute md:bottom-[60px] bottom-[60px] md:left-[30px] left-[30px] md:w-[280px] w-[200px]'
+              className='absolute md:bottom-[60px] bottom-[60px] md:left-[30px] left-[20px] md:w-[280px] w-[200px]'
               href="https://andrew-and-carolyn.wedsites.com/rsvp"
               target='_blank'
               >
                 <img src='/rsvpBtn.png'
                 className='w-[520px]'/>
               </a>
-            </motion.div>
+            </div>
             {/* signature */}
             <div className='w-full py-16 px-8'>
               <p className='text-2xl md:text-4xl font-serif'>We cant' wait to celebrate with you!</p>
